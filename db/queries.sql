c7ba62068cafd146812239993b465b01dc739777
omgitsmotrix <mohammedxec@gmail.com>
2018-02-18 01:10:57 +0000

    cleaned the styling and added slideshow.

---
+++ db/queries.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	image VARCHAR(100) NOT NULL,
+	description TEXT NOT NULL,
+	brand VARCHAR(100) NOT NULL,
+	price DECIMAL(10,2) NOT NULL
+);
